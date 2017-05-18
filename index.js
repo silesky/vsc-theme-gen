a211ed70c8b56c760668424c7b6713cdf898da23
silesky <seth.silesky@gmail.com>
2017-05-18 00:51:30 -0500

    eslint fix

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-'use strict';
+'use strict'
 
-const express = require('express');
+const express = require('express')
 
 // Constants
 const PORT = 8080
 
 // App
-const app = express();
+const app = express()
 app.get('/', function (req, res) {
-  res.send('Hello world!!!');
-});
+  res.send('Hello world!!!')
+})
 
-app.listen(PORT);
-console.log(`Running on http://localhost:${PORT}`);
+app.listen(PORT)
+console.log(`Running on http://localhost:${PORT}`)
