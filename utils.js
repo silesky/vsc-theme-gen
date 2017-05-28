bd4b388f76e71da20d9b0e66130bbeecd79b70a1
silesky <seth.silesky@gmail.com>
2017-05-28 17:18:48 -0500

    generates markup correctly

--- utils.js
+++ utils.js
@@ -3,13 +3,34 @@ const registry = new Registry()
 const JSSyntaxPath = 'JavaScript.plist'
 const grammar = registry.loadGrammarFromPathSync(JSSyntaxPath)
 
+const generateMarkup = (code, tokens) => {
+  return tokens
+    .map(el => {
+      el.code = code.slice(el.startIndex, el.endIndex) // add a new 'code' property
+      return el
+    })
+    .map(el => {
+      const classes = el.scopes
+        .join(' ')
+        .replace(/.js/gi, '') // remove , .js
+        .replace(/\./gi, '-') // replace periods with dashes
+        .replace('source', '') // remove 'source'
+        .trim() // get rid of leading and trailing spaces, but not space between
+      return `<span class='${classes}'>${el.code}</span>` // create the markup
+    })
+}
+
+/** Returns a json object that describes the grammar of the code string
+ * @param { string } code - code to tokenize
+ * @param { language } language - e.g. js, python
+ * @returns { object } representation of token
+ */
+const tokenize = (code, language) => {
+  const tokens = grammar.tokenizeLine(code).tokens
+  const markup = generateMarkup(code, tokens)
+  return markup
+}
+
 module.exports = {
-  /** Returns a json object that describes the grammar of the code string
-   * @param { string } code - code to tokenize
-   * @param { language } language - e.g. js, python
-   * @returns { object } representation of token
-   */
-  tokenize: (code, language) => {
-    return grammar.tokenizeLine(code)
-  }
+  tokenize
 }
