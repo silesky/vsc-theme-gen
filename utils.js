875fbcece07e64f455f8b238dd50a13f977726ca
silesky <seth.silesky@gmail.com>
2017-05-30 12:10:10 -0500

    replace periods with spaces instead of dashes

--- utils.js
+++ utils.js
@@ -12,7 +12,7 @@ const scopesToClasses = scopes =>
   scopes
     .join(' ')
     .replace(/.js/gi, '') // remove , .js
-    .replace(/\./gi, '-') // replace periods with dashes
+    .replace(/\./gi, ' ') // replace periods with spaces
     .replace('source', '') // remove 'source'
     .trim() // get rid of leading and trailing spaces, but not space between
 
