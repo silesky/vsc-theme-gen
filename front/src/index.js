37539d5382b5d470daf32cd95b272f493785d331
silesky <seth.silesky@gmail.com>
2017-05-31 13:04:54 -0500

    refactor, ready to actually add styles

--- front/src/index.js
+++ front/src/index.js
@@ -1,26 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import config from './config'
 import * as utils from './utils'
 class App extends React.Component {
   constructor () {
     super()
+    this.handleFormChange = this.handleFormChange.bind(this)
     this.state = {
-      tokens: []
+      tokens: [],
+      inputString: null,
     }
   }
   componentDidMount () {
+    const defaultVal = `console.log('hi')`
     utils
-      .getTokens(`console.log('hi')`)
-      .then(dataWithClassNames => this.setState({ tokens: dataWithClassNames }))
+      .getTokens(defaultVal)
+      .then(tokens => this.setState({ inputString: defaultVal, tokens }))
   }
-  onColorizeClick () {}
+
+  handleFormChange ({ target: { value } }) {
+    this.setState({ inputString: value })
+  }
+
+  onColorizeClick () {
+    utils
+      .getTokens(this.state.inputString)
+      .then(tokens => this.setState({ tokens }))
+  }
   render () {
     return (
       <div>
         <h1>Hello World!!</h1>
-        <textarea rows='5' />
-        <button onClick={() => this.onColorizeClick()}>COLOR</button>
+        <textarea rows='5' onChange={this.handleFormChange} />
+        <button type='submit' onClick={() => this.onColorizeClick()}>
+          COLOR
+        </button>
         <pre>
           {this.state.tokens.map(({ classNames, code }, ind) => {
             return <span key={ind} className={classNames}>{code}</span>
